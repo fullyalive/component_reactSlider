1db0efbc40d4f961be4f85ef57972542f37a3686
fullyalive <wogud1202@gmail.com>
2018-07-23 15:15:40 +0900

    Seperating Sliders

---
+++ react_carousel_slider/src/AnimatedSlider/AnimatedSlider.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Slider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
+import "normalize.css/normalize.css";
+import "./slider-animations.css";
+import "./styles.css";
+
+const AnimatedSlider = ({ content }) => {
+  return (
+    <div>
+      <Slider className="slider-wrapper">
+        {content.map((item, index) => (
+          <div
+            key={index}
+            className="slider-content"
+            style={{
+              background: `url('${item.image}') no-repeat center center`
+            }}
+          >
+            <div className="inner">
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+              <button>{item.button}</button>
+            </div>
+            <section>
+              <img src={item.userProfile} alt={item.user} />
+              <span>
+                Posted by <strong>{item.user}</strong>
+              </span>
+            </section>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default AnimatedSlider;
--- react_carousel_slider/src/App.js
+++ react_carousel_slider/src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
-import AwesomeSlider from "./AwesomeSlider";
-import AnimatedSlider from "./AnimatedSlider";
+import AwesomeSlider from "./AwesomeSlider/AwesomeSlider";
+import AnimatedSlider from "./AnimatedSlider/AnimatedSlider";
 
 const content = [
-  { title: "Title1", description: "Description1" },
-  { title: "Title2", description: "Description2" },
-  { title: "Title3", description: "Description3" },
-  { title: "Title4", description: "Description4" }
+  {
+    title: 'Vulputate Mollis Ultricies Fermentum Parturient',
+    description:
+      'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
+    button: 'Read More',
+    image: 'https://i.imgur.com/ZXBtVw7.jpg',
+    user: 'Luan Gjokaj',
+    userProfile: 'https://i.imgur.com/JSW6mEk.png'
+  },
+  {
+    title: 'Tortor Dapibus Commodo Aenean Quam',
+    description:
+      'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
+    button: 'Discover',
+    image: 'https://i.imgur.com/DCdBXcq.jpg',
+    user: 'Erich Behrens',
+    userProfile: 'https://i.imgur.com/0Clfnu7.png'
+  },
+  {
+    title: 'Phasellus volutpat metus',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
+    button: 'Buy now',
+    image: 'https://i.imgur.com/DvmN8Hx.jpg',
+    user: 'Bruno Vizovskyy',
+    userProfile: 'https://i.imgur.com/4KeKvtH.png'
+  }
 ];
 
 class App extends Component {
